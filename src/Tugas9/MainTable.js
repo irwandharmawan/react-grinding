6b7fd0b5acedf8fb21b4206813717941dbbfa36e
Irwan Dharmawan <lexiconrandalph@gmail.com>
2020-11-28 18:35:14 +0700

    Adding popover, Can't resolve the table spacing

--- src/Tugas9/MainTable.js
+++ src/Tugas9/MainTable.js
@@ -1,14 +1,11 @@
 import Data from './PlayersData.js';
-import {ProgressBar} from 'react-bootstrap';
-
-const styleCol={
-  paddingRight:'30px'
-}
+import {ProgressBar,Pagination} from 'react-bootstrap';
+import Example from './Popping.js'
 
 function MainTable(){
   return(
     <div>
-    <table>
+    <table style={{marginTop:'20px'}}>
       <thead>
         <tr>
           <th>#</th>
@@ -32,6 +29,27 @@ function MainTable(){
         })}
       </tbody>
     </table>
+
+    <Pagination style={{marginTop:'20px'}}>
+      <Pagination.First />
+      <Pagination.Prev />
+      <Pagination.Item active>{1}</Pagination.Item>
+      <Pagination.Ellipsis />
+
+      <Pagination.Item>{10}</Pagination.Item>
+      <Pagination.Item>{11}</Pagination.Item>
+      <Pagination.Item>{12}</Pagination.Item>
+      <Pagination.Item>{13}</Pagination.Item>
+      <Pagination.Item disabled>{14}</Pagination.Item>
+
+      <Pagination.Ellipsis />
+      <Pagination.Item>{20}</Pagination.Item>
+      <Pagination.Next />
+      <Pagination.Last />
+    </Pagination>
+
+    <Example />
+
     </div>
   )};
 export default MainTable;
